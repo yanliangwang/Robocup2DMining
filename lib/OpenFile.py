3fea2b6a9e826908a6efcc24e19db2ad3122a6cd
dy <dengyong.ai@gmail.com>
2017-04-07 22:35:56 +0800

    file

---
+++ lib/OpenFile.py
@@ -0,0 +1,18 @@
+#!/usr/bin env python3.4
+
+class   OpenFile:
+	def _init_(self, file_path):
+		self.file_path=file_path
+	def read():
+		f=open(self.file_path, 'r')
+		txt=f.read()
+		return  txt
+
+
+
+
+
+
+if  __name__=='_main_':
+	
+		
