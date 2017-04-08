976962b2590bd8099b9c9bca2260d8ec9ff84e19
dy <dengyong.ai@gmail.com>
2017-04-08 20:50:53 +0800

    update

--- lib/OpenFile.py
+++ lib/OpenFile.py
@@ -1,18 +1,36 @@
 #!/usr/bin env python3.4
 
 class   OpenFile:
-	def _init_(self, file_path):
+	def __init__(self, file_path):
 		self.file_path=file_path
-	def read(self):
-		f=open(self.file_path, 'r')
-		txt=f.read()
-		return  txt
-
-
-
-
-
-
-if  __name__=='_main_':
+	def read_rcg(self):
+		rcg=open(self.file_path+".rcg")
+		return  rcg.read()
+	def read_rcl(self):
+		rcl=open(self.file_path+".rcl")
+		return  rcl.read()
 	
+#  test
+if __name__=="__main__":
+	path="../log/201704040927-Miracle_2D_4_2-vs-YuShan2017_4_3"
+	rcl=OpenFile(path).read_rcg()
+	print(rcl)
+	
+	
+	
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 		
