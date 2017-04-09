4ed83602ec8a6e26cc5ba9ff6541497a63886ba9
dy <dengyong.ai@gmail.com>
2017-04-09 16:53:19 +0800

    update

--- lib/Kick.py
+++ lib/Kick.py
@@ -1,9 +1,17 @@
 #!/usr/bin env python3.4
+import re
 
 class  Kick:
-	def __init__(self,rcl,M_team_name=team_name):
+	def __init__(self,rcl,rcg,M_team_name=team_name):
 		self.rcl=rcl
+		self.rcg=rcg
 		self.M_team_name=M_team_name
 	
 	def get_team_kick_cycle(self):
-		
+		Kick_Cycle=list()
+		for elt in rcl:
+			kick_expr="(^d),d\sRecv\s"+self.M_team_name+"\wd\(kick"
+			if  re.search(kick_expr,elt).is not None:
+				temp=re.search(kick_expr,elt).group()
+				Kick_Cycle.append(temp)
+		return Kick_Cycle
